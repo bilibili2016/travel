023288dfb015f8a71526ebf0b3d1127fb293e974
liyr <liyr@xingyuanauto.com>
2018-05-28 15:14:39 +0800

    project init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="hello">
-    <div>home</div>
-    <div>heelo</div>
+
   </div>
 </template>
 
 <script>
-
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
