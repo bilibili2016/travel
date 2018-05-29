105504ddb00b3dd38d13e04853c6bf9d468d22af
liyr <liyr@xingyuanauto.com>
2018-05-29 11:02:23 +0800

    stylus语法

--- src/pages/home/component/Header.vue
+++ src/pages/home/component/Header.vue
@@ -20,41 +20,36 @@ export default {
 }
 </script>
 
-<style scoped lang="less">
-  @import "../../../assets/styles/variables.less";
-  .header {
-    display: flex;
-    line-height: .86rem;
-    background: #00bcd4;
-    color: #fff;
-    .header-left {
-      width: .64rem;
-      float: left;
-      .back-icon {
-        text-align: center;
-        font-size:.4rem
-      }
-    }
-    .header-input {
-      flex: 1;
-      height: .64rem;
-      line-height: .64rem;
-      margin-top: .12rem;
-      margin-left: .2rem;
-      padding-left: .2rem;
-      background: #fff;
-      border-radius: .1rem;
-      color: #ccc;
-    }
-    .header-right {
-      /*min-width: 1.04rem;*/
-      width: 1.24rem;
-      float: right;
-      text-align: center;
-      .arrow-icon{
-        font-size:.24rem;
-      }
-    }
-  }
-
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display: flex
+    line-height: $headerHeight
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      min-width: 1.04rem
+      padding: 0 .1rem
+      float: right
+      text-align: center
+      color: #fff
+      .arrow-icon
+        margin-left: -0.4rem
+        font-size: .24rem
 </style>
