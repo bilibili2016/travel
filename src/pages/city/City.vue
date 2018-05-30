a387ef415cdbea93fce64ed273980e662e5c13ae
liyr <liyr@xingyuanauto.com>
2018-05-30 14:39:27 +0800

    城市页面动态数据渲染，注意遍历对象的方式

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :hot="hotCities" :cities="cities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
@@ -12,13 +12,38 @@ import CityHeader from './component/Header.vue'
 import CitySearch from './component/Search.vue'
 import CityList from './component/List.vue'
 import CityAlphabet from './component/Alphabet.vue'
+import axios from 'axios'
 export default {
   name:'City',
+  data () {
+    return {
+      hotCities:[],
+      cities:{}
+    }
+  },
   components:{
     CityHeader,
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  mounted () {
+    this.getCityInfo()
+  },
+  methods:{
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.getCityInfoSuccess)
+    },
+    getCityInfoSuccess(res){
+      console.log(res);
+      res = res.data;
+      if(res.ret && res.data){
+        const result = res.data;
+        this.hotCities = result.hotCities
+        this.cities = result.cities
+      }
+    }
   }
 }
 </script>
